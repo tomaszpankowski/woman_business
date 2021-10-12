762c4bdb2f7c991d62a2704d61427297c803aad7
pankowskitomasz <pankowski.email@gmail.com>
2021-10-12 19:16:57 +0200

    project's initial state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: [
+    './app.component.sass',
+    './sass/styles.scss'
+    ]
 })
 export class AppComponent {
   title = 'project';
